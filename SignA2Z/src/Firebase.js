82aec339697c9be7f70d9791516b444732862ccd
KManipis <kurtmanipis11@gmail.com>
2025-09-08 02:48:13 +0000

    firebasejs

--- SignA2Z/src/Firebase.js
+++ SignA2Z/src/Firebase.js
@@ -1,16 +1,21 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAq-cmiw_pHauINcYQnLdhVOyhGqlWkBRc",
   authDomain: "signala2z.firebaseapp.com",
   projectId: "signala2z",
-  storageBucket: "signala2z.appspot.com",
+  storageBucket: "signala2z.firebasestorage.app",
   messagingSenderId: "724997183385",
-  appId: "1:724997183385:web:abcdef123456", // 👈 grab this from Firebase Console (Web app config)
+  appId: "1:724997183385:web:1edc6a9b789b7ea167a508",
+  measurementId: "G-G8D2J46CRD"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
